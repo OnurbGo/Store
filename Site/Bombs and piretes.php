cf4268e04570e8d7d574bc614e8c1ae2b53c0a88
OnurbGo <bruno.baltieri@grupointegrado.br>
2024-05-28 22:20:18 -0300

    Meia Lua Store V0.7

---
+++ Site/Bombs and piretes.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Meia-Lua Store</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="shortcut icon" href="Imagens/Icone.png" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+  <body>
+    <header class="header">
+      <a href="index.php" title="Home" class="header-logo">
+        <img
+          class="logo" src="Imagens/Logo Meia Lua.png" alt="Logo" title="Logo"
+        />
+      </a>
+
+      <nav class="header-nav">
+        <ul>
+          <li><a href="index.php" title="Home">Home</a></li>
+          <li><a href="Loja.php" title="Loja">Loja</a></li>
+          <li><a href="Contato.php" title="Contato">Contato</a></li>
+          <li><a href="Equipe.php" title="Equipe">Equipe</a></li>
+        </ul>
+      </nav>
+    </header>
+  
+    <img
+    class="banner"
+    src="Imagens/Banner Piratas.png"
+    alt="Piratas E Bombas"
+    title="Piratas E Bombas"
+    />
+
+    <main>
+      <h1 class="h1">Piratas</h1>
+
+    
+      <div class="conteiner-capa">
+      <img class="capa" src='Imagens/Capa.png'/>
+        <p>
+        Você presencia Alexander Springovisk em ação atraves de cameras de segurança, 
+        ele é um agente altamente habilidoso e capacitado para resolver qualquer missão 
+        que caia em sua responsabilidade.
+        <br><br>
+
+        Seu personagem é um agente especial de missões
+        extra-officiais, extremamente eficiente em seus metodos, 
+        e seu lema é missão dada é missão cumprida.
+        <br><br>
+
+        Sua missão é Eliminar um politico envolvido com organizações criminosas, 
+        este homem é mandante de mais crimes que você se quer consiguiria pensar.
+        <br><br>
+
+        Sua missão é Eliminar um politico envolvido com organizações criminosas, 
+        este homem é mandante de mais crimes que você se quer consiguiria pensar 
+        <br> <br>
+        </p>
+      </div>
+
+      <br><br><br>
+
+
+      <!--<div class="conteiner-centro">
+      <img class="gif-alex" src="imagens/andar.gif" alt="Gif Andando">
+      <br><br>-->
+      
+      <iframe class="jogocentro" width="90%" height="500" src="Jogos/bomb and pirates/index.html" title="Jogo"></iframe>
+
+      <br><br>
+      <ul class="textocentro">
+        <li>Ficha Tecnica</li>
+        <br>
+        <li><strong>Engine:</strong>	Construct 3</li>
+        <li><strong>Plataforma:</strong> PC</li>
+        <li><strong>Mecânica:</strong> shooter, Top Down View</li>
+        <li><strong>Dispositivos de Entrada:</strong> Mouse, Teclado</li>
+        </ul>
+        <br><br><br><br><br><br>
+        </div>
+      
+
+    </main>
+
+  </body>
+
+  <footer class="footer">
+    <img
+      class="logocybertech"
+      src="Imagens/Logo Cybertech.png"
+      alt="logo cybertech"
+      title="logo cybertech"
+    />
+    <p class="textobranco">Created By Cybertech</p>
+    <p class="textobranco">Todos os direitos reservados a Bruno Gotardo</p>
+  </footer>
+</html>
---
+++ Site/Loja.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Meia-Lua Store</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="shortcut icon" href="Imagens/Icone.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <header class="header">
+      <a href="index.php" title="Home" class="header-logo">
+        <img
+          class="logo" src="Imagens/Logo Meia Lua.png" alt="Logo" title="Logo"
+        />
+      </a>
+
+      <nav class="header-nav">
+        <ul>
+          <li><a href="index.php" title="Home">Home</a></li>
+          <li><a href="Loja.php" title="Loja">Loja</a></li>
+          <li><a href="Contato.php" title="Contato">Contato</a></li>
+          <li><a href="Equipe.php" title="Equipe">Equipe</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  <!--Inicio Do Carrocel-->
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <a href="Parabellum.php">
+    <img class="banner" src="Imagens/Banner Parabellum.png" class="d-block w-100" alt="Banner Parabellum">
+    </a>
+  </div>
+    <div class="carousel-item">
+    <a href="Bombs and piretes.php">
+    <img class="banner" src="Imagens/Banner Piratas.png" class="d-block w-100" alt="Banner Pirata">
+    </a>
+    </div>
+      <div class="carousel-item">
+        <img class="banner" src="..." class="d-block w-100" alt="Banner Jogo 3">
+      </div>
+    </div>
+    <!--Fim Do Carrocel-->
+
+    <!--Inicio Controles-->
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden"><</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">></span>
+    </button>
+  </div>
+  <!--Fim Controles-->
+
+
+    <main class="container">
+      <h1>Produtos Em Destaque</h1>
+      <div class="flex">
+        <div class="flex-coluna">
+          <a class="textoflex" href="Parabellum.php">
+            <img src="Imagens/Capa.png"alt="Parabellum" title="Parabellum">
+            <p class="flexp">Parabellum</p>
+          </a>
+        </div>
+        <div class="flex-coluna">
+          <a class="textoflex" href="Bombs and piretes.php">
+          <img src="Imagens/Capa Piratas.jpeg" alt="Piratas E Bombas" title="Piratas E Bombas">
+          <p class="flexp">Jogo2</p>
+          </a>
+        </div>
+        <div class="flex-coluna">
+          <img src="" alt="Jogo3" title="Jogo3">
+          <p class="flexp">Jogo3</p>
+        </div>
+        <div class="flex-coluna">
+          <img src="" alt="Jogo4" title="Jogo4" />
+          <p class="flexp">Jogo4</p>
+        </div>
+      </div>
+    </main>
+
+    <br><br><br><br><br><br>
+  </body>
+
+  <footer class="footer">
+    <img
+      class="logocybertech"
+      src="Imagens/Logo Cybertech.png"
+      alt="logo cybertech"
+      title="logo cybertech"
+    />
+    <p class="textobranco">Created By Cybertech</p>
+    <p class="textobranco">Todos os direitos reservados a Bruno Gotardo</p>
+  </footer>
+</html>
